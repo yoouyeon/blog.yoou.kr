d527b6fbc4f94dae138b39602aeeda7ebff6e91f
yoouyeon <jyeon.yoon59@gmail.com>
2023-06-25 19:43:58 +0900

    [Style] 헤더 스타일 변경

--- src/components/main/Layout.styles.tsx
+++ src/components/main/Layout.styles.tsx
@@ -1,35 +1,46 @@
 // import styled from "@emotion/styled";
 import { css } from "@emotion/react";
+// import * as colors from "../../styles/colors.styles.ts";
+
+const LIGHT_PAGE_BACKGROUND = "#D2E4FF";
+const LIGHT_COMPONENT_BACKGROUND = "#FDFCFF";
+
+const MOBILE_MEDIA_QUERY = "@media (max-width: 640px)";
+
+const ComponentBackground = css({
+  background: LIGHT_COMPONENT_BACKGROUND,
+  borderRadius: "24px",
+});
 
 export const LayoutContainer = (isRootPath: boolean) =>
   css({
+    padding: "7.5rem 8.75rem",
+    background: LIGHT_PAGE_BACKGROUND,
     display: "grid",
+    gridGap: "1.25rem",
     gridTemplateColumns: "repeat(4, 1fr)",
     gridTemplateAreas:
       '"LayoutSideBar LayoutHeader LayoutHeader LayoutHeader" \
       "LayoutSideBar LayoutMain LayoutMain LayoutMain" \
       "LayoutFooter LayoutFooter LayoutFooter LayoutFooter"',
-    "@media (max-width: 991.98px)": {
+    MOBILE_MEDIA_QUERY: {
       gridTemplateColumns: "1fr",
       gridTemplateAreas:
         '"LayoutHeader" \
         "LayoutSideBar" \
-        "LayoutAdmonition" \
         "LayoutMain" \
         "LayoutFooter"',
     },
   });
 
 export const LayoutHeader = (isRootPath: boolean) =>
-  css({
+  css(ComponentBackground, {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    background: "#9BCDD2",
-    height: `${isRootPath ? "15rem" : "10rem"}`,
-    // height: "15rem",
-    fontFamily: "Jalnan",
-    fontSize: "3rem",
+    height: "4rem",
+    fontFamily: "Cafe24 Ssurround",
+    fontSize: "1.75rem",
     "> a": {
       color: "#000000",
       border: "none",
@@ -37,27 +48,18 @@ export const LayoutHeader = (isRootPath: boolean) =>
     gridArea: "LayoutHeader",
   });
 
-export const LayoutAdmonition = css({
-  display: "none",
-  gridArea: "LayoutAdmonition",
-  width: "100%",
-  "@media (max-width: 991.98px)": {
-    display: "block",
-  },
-});
-
 export const LayoutSideBar = css({
   gridArea: "LayoutSideBar",
-  background: "#F9F5F6",
-  padding: "2.75rem",
+  // background: "#F9F5F6",
+  // padding: "2.75rem",
 });
 
 export const LayoutMain = css({
   gridArea: "LayoutMain",
-  padding: "3.75rem 9.4rem",
-  "@media (max-width: 991.98px)": {
-    padding: "2rem 1.25rem",
-  },
+  // padding: "3.75rem 9.4rem",
+  // MOBILE_MEDIA_QUERY: {
+  //   padding: "2rem 1.25rem",
+  // },
 });
 
 export const LayoutFooter = css({
