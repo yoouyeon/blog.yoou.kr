128beb3ae903b5dc57d7e0d58f51dded3b192188
yoouyeon <jyeon.yoon59@gmail.com>
2023-06-25 20:53:48 +0900

    [Style] Layout width 수정

--- src/components/main/Layout.styles.tsx
+++ src/components/main/Layout.styles.tsx
@@ -1,28 +1,26 @@
-// import styled from "@emotion/styled";
 import { css } from "@emotion/react";
-// import * as colors from "../../styles/colors.styles.ts";
 
-const LIGHT_PAGE_BACKGROUND = "#D2E4FF";
 const LIGHT_COMPONENT_BACKGROUND = "#FDFCFF";
 
 const MOBILE_MEDIA_QUERY = "@media (max-width: 640px)";
 
-const ComponentBackground = css({
+export const ComponentBackground = css({
   background: LIGHT_COMPONENT_BACKGROUND,
   borderRadius: "24px",
 });
 
 export const LayoutContainer = (isRootPath: boolean) =>
   css({
-    padding: "7.5rem 8.75rem",
-    background: LIGHT_PAGE_BACKGROUND,
+    padding: "7.5rem 0",
+    margin: "0 auto",
+    maxWidth: "1080px",
     display: "grid",
     gridGap: "1.25rem",
-    gridTemplateColumns: "repeat(4, 1fr)",
+    gridTemplateColumns: "240px calc(100% - 1 * 240px)",
     gridTemplateAreas:
-      '"LayoutSideBar LayoutHeader LayoutHeader LayoutHeader" \
-      "LayoutSideBar LayoutMain LayoutMain LayoutMain" \
-      "LayoutFooter LayoutFooter LayoutFooter LayoutFooter"',
+      '"LayoutSideBar LayoutHeader" \
+      "LayoutSideBar LayoutMain" \
+      "LayoutFooter LayoutFooter"',
     MOBILE_MEDIA_QUERY: {
       gridTemplateColumns: "1fr",
       gridTemplateAreas:
@@ -40,7 +38,7 @@ export const LayoutHeader = (isRootPath: boolean) =>
     alignItems: "center",
     height: "4rem",
     fontFamily: "Cafe24 Ssurround",
-    fontSize: "1.75rem",
+    fontSize: "2rem",
     "> a": {
       color: "#000000",
       border: "none",
@@ -50,16 +48,10 @@ export const LayoutHeader = (isRootPath: boolean) =>
 
 export const LayoutSideBar = css({
   gridArea: "LayoutSideBar",
-  // background: "#F9F5F6",
-  // padding: "2.75rem",
 });
 
 export const LayoutMain = css({
   gridArea: "LayoutMain",
-  // padding: "3.75rem 9.4rem",
-  // MOBILE_MEDIA_QUERY: {
-  //   padding: "2rem 1.25rem",
-  // },
 });
 
 export const LayoutFooter = css({
--- src/styles/Layout.styles.ts
+++
@@ -1,9 +0,0 @@
-import { css } from "@emotion/react";
-
-export const layoutTitle = css({
-  fontFamily: "Jalnan",
-  fontSize: "2.25rem",
-  fontWeight: "900",
-  color: "#000000",
-  textShadow: "0rem 0rem 0.5rem #FF78C4",
-});
