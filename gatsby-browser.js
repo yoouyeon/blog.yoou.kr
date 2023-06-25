4027ec36ddd85cc07f44943c30a9d2d0f2bbe3e0
yoouyeon <jyeon.yoon59@gmail.com>
2023-06-26 03:08:03 +0900

    [Style] 기본 디자인 파일 제거

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,6 @@ import "@fontsource/nanum-gothic-coding";
 import "./src/normalize.css";
 // custom CSS styles
 import "./src/style.css";
-import "./src/custom.scss";
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css";
